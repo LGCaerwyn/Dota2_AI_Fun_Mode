6972264599a24cfae4dddf63b32c25c65875ee6d
LGCaerwyn <1530491720@qq.com>
2025-04-18 21:00:32 +0800

    fun skill spellicons
    recolor some spellicons

--- scripts/vscripts/fun_heroes/hero_axe/counter_helix.lua
+++ scripts/vscripts/fun_heroes/hero_axe/counter_helix.lua
@@ -49,53 +49,46 @@ function CounterHelix( keys )
 
 
 
-			if target == caster and not caster:HasModifier(helix_modifier) then
+		if target == caster and not caster:HasModifier(helix_modifier) then
 
-				ability:ApplyDataDrivenModifier(caster, caster, helix_modifier, {})
-				
-						
-						if PlayerResource:IsFakeClient(caster:GetPlayerID()) and caster:HasScepter() then
-						   ability:ApplyDataDrivenModifier(caster, caster, "modifier_counter_helix_fun_buff", {duration = 20 })
-						    --x娴嬭瘯娌℃湁鏁堟灉
-						end
-						--路麓禄梅脗脻脨媒脡脣潞娄
-							local targets = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, 350, 
-					            DOTA_UNIT_TARGET_TEAM_ENEMY, 
-					            DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC , 
-					            DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES , FIND_ANY_ORDER  , false)
-
-							for k,target in pairs(targets) do
-							    --print(k,v)
-							    damagetable = {
-							    victim = target,
-							    attacker = caster, 
-							    damage = damage,
-							    damage_type = damage_type,
-							    ability  =  ability
-							       		  }
-							    ApplyDamage(damagetable)
-								--脮露脡卤虏芒脢脭
-
-					--[[]		
-						if PlayerResource:IsFakeClient(caster:GetPlayerID())  then
-
-								if caster:HasScepter() and target:IsRealHero() and target:GetHealth() < damage_axe then
-									caster:SetCursorCastTarget(target)
-									tether:OnSpellStart()
-								end
-						end
-						   --]]
-				           	end
-				               -- print("脰麓脨脨脡脣潞娄")
-
-				        
-				ability:StartCooldown(cooldown)
+			ability:ApplyDataDrivenModifier(caster, caster, helix_modifier, {})
+			
+					
+			if PlayerResource:IsFakeClient(caster:GetPlayerID()) and caster:HasScepter() then
+			   ability:ApplyDataDrivenModifier(caster, caster, "modifier_counter_helix_fun_buff", {duration = 20 })
+				--x娴嬭瘯娌℃湁鏁堟灉
 			end
+			--路麓禄梅脗脻脨媒脡脣潞娄
+			local targets = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, 350, 
+				DOTA_UNIT_TARGET_TEAM_ENEMY, 
+				DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC , 
+				DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES , FIND_ANY_ORDER  , false)
+
+			for k,target in pairs(targets) do
+				--print(k,v)
+				damagetable = {
+				victim = target,
+				attacker = caster, 
+				damage = damage,
+				damage_type = damage_type,
+				ability  =  ability
+						  }
+				ApplyDamage(damagetable)
+				--脮露脡卤虏芒脢脭
 
 
+				if PlayerResource:IsFakeClient(caster:GetPlayerID())  then
 
+					if caster:HasScepter() and target:IsRealHero() and target:GetHealth() < damage_axe then
+						caster:SetCursorCastTarget(target)
+						tether:OnSpellStart()
+					end
+				end
+			end
+				   -- print("脰麓脨脨脡脣潞娄")
+			ability:StartCooldown(cooldown)
+		end
 	end
-
 end
 
 
